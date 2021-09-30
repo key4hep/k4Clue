1b15b7ef163d445fe37278511347a5c00ad66111
Erica Brondolin <erica.brondolin@cern.ch>
2021-09-30 16:16:56 +0200

    Start including some histos

--- src/Validation.cc
+++ src/Validation.cc
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <TString.h>
+#include <TH1F.h>
+#include <TFile.h>
+
 //EDM4HEP libraries
 #include "edm4hep/ClusterCollection.h"
 #include "podio/ROOTReader.h"
@@ -9,35 +13,55 @@ using namespace dd4hep ;
 using namespace DDSegmentation ;
 
 std::string bitFieldCoder = "system:0:5,side:5:-2,module:7:8,stave:15:4,layer:19:9,submodule:28:4,x:32:-16,y:48:-16" ;
+TH1F* h_clusters = new TH1F("Num_clusters","Num_clusters",100, 0, 100);
+TH1F* h_clEnergy = new TH1F("Energy","Energy",100, 0, 0.100);
+TH1F* h_clSize   = new TH1F("Size","Size",100, 0, 100);
+TH1F* h_clustersLayer = new TH1F("Num_clusters_layer","Num_clusters_layer",100, 0, 100);
+TH1F* h_clHitsLayer   = new TH1F("Num_clHits_layer","Num_clHits_layer",100, 0, 100);
+TH1F* h_clEnergyLayer = new TH1F("Energy_layer","Energy_layer",100, 0, 100);
 
-void read_ClustersEDM4HEP_event(const edm4hep::ClusterCollection& cl){
+void read_ClustersEDM4HEP_event(const edm4hep::ClusterCollection& cls){
 
-  for (const auto& ch : cl) {
-    for (const auto& hit : ch.getHits()) {
+  h_clusters->Fill(cls.size());
+  for (const auto& cl : cls) {
+    
+    h_clEnergy->Fill(cl.getEnergy());
+    h_clSize->Fill(cl.getHits().size());
+    std::cout << cl.getHits().size() << " caloHits in this cluster" << std::endl;
+
+    int ch_layer = 0;
+    for (const auto& hit : cl.getHits()) {
       const BitFieldCoder bf(bitFieldCoder) ;
-      auto ch_layer = bf.get( hit.getCellID(), "layer");
+      ch_layer = bf.get( hit.getCellID(), "layer");
       auto ch_energy = hit.getEnergy();
       std::cout << "  " << ch_layer << std::endl;
+      h_clEnergyLayer->Fill(ch_layer, hit.getEnergy());
+      h_clHitsLayer->Fill(ch_layer);
     }
-    std::cout << ch.getHits().size() << " caloHits in this cluster" << std::endl;
+    h_clustersLayer->Fill(ch_layer);
+
   }
 
 }
 
 int main(int argc, char *argv[]) {
 
-  std::string inputFileName = "";
+  std::string inputFileName  = "";
+  TString outputFileName = "";
   std::string clusterCollectionName = "";
-  if (argc == 3) {
+  if (argc == 4) {
     inputFileName = argv[1];
-    clusterCollectionName = argv[2];
+    outputFileName = argv[2];
+    clusterCollectionName = argv[3];
   } else {
-    std::cout << "./validation [fileName] [collectionName]" << std::endl;
+    std::cout << "./validation [fileName] [outputFileName] [collectionName]" << std::endl;
     return 1;
   }
 
   // Read EDM4HEP data
   if(inputFileName.find(".root")!=std::string::npos){
+
+    TFile file((outputFileName+".root"),"recreate");
 
     std::cout<<"input edm4hep file: "<<inputFileName<<std::endl;
     podio::ROOTReader reader;
@@ -62,6 +86,14 @@ int main(int argc, char *argv[]) {
       reader.endOfEvent();
     }
     reader.closeFile();
+
+    h_clusters->Write();
+    h_clSize->Write();
+    h_clEnergy->Write();
+    h_clustersLayer->Write();
+    h_clHitsLayer->Write();
+    h_clEnergyLayer->Write();
+    file.Close();
 
   }
 
