71464c8680d85778f456a44d27101d730e0a4d19
Erica Brondolin <erica.brondolin@cern.ch>
2022-03-30 12:08:33 +0200

    Extend class with other members

--- include/CLUECalorimeterHit.h
+++ include/CLUECalorimeterHit.h
@@ -11,12 +11,19 @@ public:
   using CalorimeterHit::CalorimeterHit;
 
   /// constructors
-  CLUECalorimeterHit(const CalorimeterHit& ch);// : CalorimeterHit(ch) {}
+  CLUECalorimeterHit(const CalorimeterHit& ch);
 
-  CLUECalorimeterHit(const CalorimeterHit& ch, const int layer, const float rho, const float delta);
+  CLUECalorimeterHit(const CalorimeterHit& ch, const int layer, const bool inBarrel, 
+                     const bool isSeed, const float rho, const float delta);
 
   /// Access the layer number
   const std::uint64_t& getLayer() const;
+
+  /// Access the part of calorimeter
+  const bool& inBarrel() const;
+
+  /// Access the seed value
+  const bool& isSeed() const;
 
   /// Access the delta
   const float& getDelta() const;
@@ -24,10 +31,29 @@ public:
   /// Access the rho
   const float& getRho() const;
 
+  /// Access the transverse position
+  const float& getR() const;
+
+  /// Access the eta
+  const float& getEta() const;
+
+  /// Access the phi
+  const float& getPhi() const;
+
+  /// Set hit transverse global position, pseudorapidity and phi
+  void setR();
+  void setEta();
+  void setPhi();
+
 private:
   std::uint64_t m_layer{};
+  bool  m_inBarrel{};
+  bool  m_isSeed{};
   float m_rho{};
   float m_delta{};
+  float m_r{};
+  float m_eta{};
+  float m_phi{};
 };
 
 } // namespace clue
--- src/CLUECalorimeterHit.cpp
+++ src/CLUECalorimeterHit.cpp
@@ -1,18 +1,47 @@
 #include "CLUECalorimeterHit.h"
+#include <cmath>
 
 namespace clue{
 
 CLUECalorimeterHit::CLUECalorimeterHit(const CalorimeterHit& ch)
-  : CalorimeterHit(ch) {}
+  : CalorimeterHit(ch) {
+  setR();
+  setEta();
+  setPhi();
+}
 
-CLUECalorimeterHit::CLUECalorimeterHit(const CalorimeterHit& ch, const int layer, const float rho, const float delta)
+CLUECalorimeterHit::CLUECalorimeterHit(const CalorimeterHit& ch, const int layer, const bool inBarrel,
+                                       const bool isSeed, const float rho, const float delta)
   : CalorimeterHit(ch),
     m_layer(layer),
+    m_inBarrel(inBarrel),
+    m_isSeed(isSeed),
     m_rho(rho),
-    m_delta(delta) {}
+    m_delta(delta) {
+  setR();
+  setEta();
+  setPhi();
+}
 
 const std::uint64_t& CLUECalorimeterHit::getLayer() const { return m_layer; }
+const bool&  CLUECalorimeterHit::inBarrel() const { return m_inBarrel; }
+const bool&  CLUECalorimeterHit::isSeed() const { return m_isSeed; }
 const float& CLUECalorimeterHit::getRho() const { return m_rho; }
 const float& CLUECalorimeterHit::getDelta() const { return m_delta; }
+const float& CLUECalorimeterHit::getR() const { return m_r; }
+const float& CLUECalorimeterHit::getEta() const { return m_eta; }
+const float& CLUECalorimeterHit::getPhi() const { return m_phi; }
+
+void CLUECalorimeterHit::setEta() { 
+  m_eta = - 1. * log(tan(atan2(m_r, getPosition().z)/2.));
+}
+
+void CLUECalorimeterHit::setPhi() {
+  m_phi = atan2(getPosition().y, getPosition().x);
+}
+
+void CLUECalorimeterHit::setR() { 
+  m_r = float(sqrt(getPosition().x*getPosition().x + getPosition().y*getPosition().y));
+}
 
 }
