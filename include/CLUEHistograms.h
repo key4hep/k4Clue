5f84006050fa87c03aae83a2a402b6c98c4e872e
Erica Brondolin <erica.brondolin@cern.ch>
2022-04-01 17:47:33 +0200

    Add clusters plot in analysis

--- include/CLUEHistograms.h
+++ include/CLUEHistograms.h
@@ -5,6 +5,10 @@
 #include "GaudiAlg/GaudiAlgorithm.h"
 #include "GaudiKernel/ITHistSvc.h"
 
+#include <edm4hep/CalorimeterHitCollection.h>
+#include <edm4hep/ClusterCollection.h>
+#include "CLUECalorimeterHit.h"
+
 #include "TH1F.h"
 
 class CLUEHistograms : public GaudiAlgorithm {
@@ -20,9 +24,18 @@ public:
   virtual StatusCode finalize();
 
 private:
+  const clue::CLUECalorimeterHitCollection* clue_calo_coll;
+  std::string ClusterCollectionName;
+  const edm4hep::ClusterCollection* cluster_coll; 
+
   ITHistSvc* m_ths{nullptr};  ///< THistogram service
   TH1F* h_clusters{nullptr};
+  TH1F* h_clSize{nullptr};
+  TH1F* h_clEnergy{nullptr};
+  TH1F* h_clLayer{nullptr};
+  TH1F* h_clHitsLayer{nullptr};
+  TH1F* h_clHitsEnergyLayer{nullptr};
 
 };
 
-#endif  // GENERATION_HEPMCHISTOGRAMS_H
+#endif  // CLUE_HISTOGRAMS_H
