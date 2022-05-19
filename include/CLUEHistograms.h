42759de89baa36b1a780ecc1a40d6d8bf8971dcc
Erica Brondolin <erica.brondolin@cern.ch>
2022-05-19 20:17:15 +0200

    Introduce tree for clusters and clean up (#28)

--- include/CLUEHistograms.h
+++ include/CLUEHistograms.h
@@ -20,25 +20,33 @@ public:
   CLUEHistograms(const std::string& name, ISvcLocator* svcLoc);
   /// Destructor.
   ~CLUEHistograms() {
-    delete m_event;
-    delete m_region;
-    delete m_layer;
-    delete m_status;
-    delete m_x;
-    delete m_y;
-    delete m_z;
-    delete m_eta;
-    delete m_phi;
-    delete m_rho;
-    delete m_delta;
-    delete m_energy;
+    delete m_hits_event;
+    delete m_hits_region;
+    delete m_hits_layer;
+    delete m_hits_status;
+    delete m_hits_x;
+    delete m_hits_y;
+    delete m_hits_z;
+    delete m_hits_eta;
+    delete m_hits_phi;
+    delete m_hits_rho;
+    delete m_hits_delta;
+    delete m_hits_energy;
+
+    delete m_clusters_event;
+    delete m_clusters_layer;
+    delete m_clusters_size;
+    delete m_clusters_x;
+    delete m_clusters_y;
+    delete m_clusters_z;
+    delete m_clusters_energy;
   };
   /// Initialize.
   virtual StatusCode initialize();
   /// Initialize tree.
-  void initializeTree();
+  void initializeTrees();
   /// Clean tree.
-  void cleanTree();
+  void cleanTrees();
   /// Execute.
   virtual StatusCode execute();
   /// Finalize.
@@ -55,31 +63,31 @@ private:
   PodioDataSvc* m_podioDataSvc;
 
   ITHistSvc* m_ths{nullptr};  ///< THistogram service
-  TH1F* h_clusters{nullptr};
-  TH1F* h_clSize{nullptr};
-  TH1F* h_clEnergy{nullptr};
-  TH1F* h_clLayer{nullptr};
-  TH1F* h_clHitsLayer{nullptr};
-  TH1F* h_clHitsEnergyLayer{nullptr};
 
   TTree* t_hits{nullptr};
-  std::vector<int> *m_event = nullptr;
-  std::vector<int> *m_region = nullptr;
-  std::vector<int> *m_layer = nullptr;
-  std::vector<int> *m_status = nullptr;
-  std::vector<float> *m_x = nullptr;
-  std::vector<float> *m_y = nullptr;
-  std::vector<float> *m_z = nullptr;
-  std::vector<float> *m_eta = nullptr;
-  std::vector<float> *m_phi = nullptr;
-  std::vector<float> *m_rho = nullptr;
-  std::vector<float> *m_delta = nullptr;
-  std::vector<float> *m_energy = nullptr;
+  std::vector<int> *m_hits_event = nullptr;
+  std::vector<int> *m_hits_region = nullptr;
+  std::vector<int> *m_hits_layer = nullptr;
+  std::vector<int> *m_hits_status = nullptr;
+  std::vector<float> *m_hits_x = nullptr;
+  std::vector<float> *m_hits_y = nullptr;
+  std::vector<float> *m_hits_z = nullptr;
+  std::vector<float> *m_hits_eta = nullptr;
+  std::vector<float> *m_hits_phi = nullptr;
+  std::vector<float> *m_hits_rho = nullptr;
+  std::vector<float> *m_hits_delta = nullptr;
+  std::vector<float> *m_hits_energy = nullptr;
 
-  bool saveEachEvent{false};
+  TTree* t_clusters{nullptr};
+  std::vector<int> *m_clusters_event = nullptr;
+  std::vector<int> *m_clusters_layer = nullptr;
+  std::vector<int> *m_clusters_size = nullptr;
+  std::vector<float> *m_clusters_x = nullptr;
+  std::vector<float> *m_clusters_y = nullptr;
+  std::vector<float> *m_clusters_z = nullptr;
+  std::vector<float> *m_clusters_energy = nullptr;
+
   std::int32_t evNum;
-  std::vector<std::string> graphPosNames{}; 
-  std::map<const std::int32_t, std::vector<TGraph*>> graphPos{};
 };
 
 #endif  // CLUE_HISTOGRAMS_H
--- src/CLUEHistograms.cpp
+++ src/CLUEHistograms.cpp
@@ -9,7 +9,6 @@ using namespace DDSegmentation ;
 DECLARE_COMPONENT(CLUEHistograms)
 
 CLUEHistograms::CLUEHistograms(const std::string& name, ISvcLocator* svcLoc) : GaudiAlgorithm(name, svcLoc), m_eventDataSvc("EventDataSvc", "CLUEHistograms") {
-  declareProperty("SaveEachEvent", saveEachEvent, "Allow to save further plots per event");
   declareProperty("ClusterCollection", ClusterCollectionName, "Collection of clusters in input");
   StatusCode sc = m_eventDataSvc.retrieve();
 }
@@ -28,74 +27,23 @@ StatusCode CLUEHistograms::initialize() {
     return StatusCode::FAILURE;
   }
 
-  h_clusters = new TH1F("Clusters","Clusters",100, 0, 100);
-  if (m_ths->regHist("/rec/Clusters", h_clusters).isFailure()) {
-    error() << "Couldn't register Clusters hist" << endmsg;
-  }
-
-  h_clSize = new TH1F("ClustersSize","ClusterSize",100, 0, 100);
-  if (m_ths->regHist("/rec/ClusterSize", h_clSize).isFailure()) {
-    error() << "Couldn't register ClusterSize hist" << endmsg;
-  }
-
-  h_clEnergy = new TH1F("ClusterEnergy","ClusterEnergy",100, 0, 0.100);
-  if (m_ths->regHist("/rec/ClusterEnergy", h_clEnergy).isFailure()) {
-    error() << "Couldn't register ClusterEnergy hist" << endmsg;
-  }
-
-  h_clLayer = new TH1F("ClusterLayer","ClusterLayer",100, 0, 100);
-  if (m_ths->regHist("/rec/ClusterLayer", h_clLayer).isFailure()) {
-    error() << "Couldn't register ClusterLayer hist" << endmsg;
-  }
-
-  h_clHitsLayer = new TH1F("ClusterHitsLayer","ClusterHitsLayer",100, 0, 100);
-  if (m_ths->regHist("/rec/ClusterHitsLayer", h_clHitsLayer).isFailure()) {
-    error() << "Couldn't register ClusterHitsLayer" << endmsg;
-  }
-
-  h_clHitsEnergyLayer = new TH1F("ClusterHitsEnergy_layer","ClusterHitsEnergy_layer",100, 0, 100);
-  if (m_ths->regHist("/rec/ClusterHitsEnergy_layer", h_clHitsEnergyLayer).isFailure()) {
-    error() << "Couldn't register ClusterHitsEnergy_layer hist" << endmsg;
-  }
-
-  graphPosNames = {"Pos_clusters_XY", "Pos_clusters_YZ", "Pos_clusters_RZ",
-                   "Pos_clusterHits_XY", "Pos_clusterHits_YZ", "Pos_clusterHits_RZ"
-                   };
-
   t_hits = new TTree ("hits", "CLUE calo hits ntuple");
   if (m_ths->regTree("/rec/NtuplesHits", t_hits).isFailure()) {
     error() << "Couldn't register hits tree" << endmsg;
   }
 
-  initializeTree();
+  t_clusters = new TTree ("clusters", "Clusters ntuple");
+  if (m_ths->regTree("/rec/NtuplesClusters", t_clusters).isFailure()) {
+    error() << "Couldn't register clusters tree" << endmsg;
+  }
+
+  initializeTrees();
 
   return StatusCode::SUCCESS;
 }
 
 StatusCode CLUEHistograms::execute() {
   info() << "CLUEHistograms::execute()" << endmsg;
-
-  if(saveEachEvent){
-    warning() << "Careful! You will be saving more plots than you actually want!" << endmsg;
-
-    DataHandle<edm4hep::EventHeaderCollection> ev_handle {
-      "EventHeader", Gaudi::DataHandle::Reader, this};
-    auto evs = ev_handle.get();
-    evNum = (*evs)[0].getEventNumber();
-    info() << "Event number = " << evNum << endmsg;
-
-    for (auto iName : graphPosNames) {
-      std::string nameGraphInFolder = "/rec/Event" + std::to_string(evNum) + "/" + iName;
-      TGraph* gr = new TGraph();
-      graphPos[evNum].push_back(gr);
-      gr->SetName(TString(iName));
-      if (m_ths->regGraph(nameGraphInFolder, gr).isFailure()) {
-        error() << "Couldn't register " << nameGraphInFolder << endmsg;
-      }
-
-    }
-
-  } // endif saveEachEvent
 
   DataObject* pStatus  = nullptr;
   StatusCode  scStatus = eventSvc()->retrieveObject("/Event/CLUECalorimeterHitCollection", pStatus);
@@ -117,73 +65,62 @@ StatusCode CLUEHistograms::execute() {
   const BitFieldCoder bf(cellIDstr);
   std::cout << cellIDstr << std::endl;
 
+  cleanTrees();
+
   std::uint64_t ch_layer = 0;
   std::uint64_t nClusters = 0;
   std::uint64_t nClusterHits = 0;
 
-  h_clusters->Fill(cluster_coll->size());
   for (const auto& cl : *cluster_coll) {
+    m_clusters_event->push_back (evNum);
+    m_clusters_energy->push_back (cl.getEnergy());
+    m_clusters_size->push_back (cl.hits_size());
 
-    h_clEnergy->Fill(cl.getEnergy());
-    h_clSize->Fill(cl.hits_size());
-    if(saveEachEvent){
-      double r = sqrt(cl.getPosition().x*cl.getPosition().x + cl.getPosition().y*cl.getPosition().y);
-      graphPos[evNum][0]->SetPoint(nClusters, cl.getPosition().y, cl.getPosition().x);
-      graphPos[evNum][1]->SetPoint(nClusters, cl.getPosition().z, cl.getPosition().y);
-      graphPos[evNum][2]->SetPoint(nClusters, cl.getPosition().z, r);
-    }
+    m_clusters_x->push_back (cl.getPosition().x);
+    m_clusters_y->push_back (cl.getPosition().y);
+    m_clusters_z->push_back (cl.getPosition().z);
 
     for (const auto& hit : cl.getHits()) {
       ch_layer = bf.get( hit.getCellID(), "layer");
-      h_clHitsLayer->Fill(ch_layer);
-      h_clHitsEnergyLayer->Fill(ch_layer, hit.getEnergy());
-      if(saveEachEvent){
-        double r = sqrt(hit.getPosition().x*hit.getPosition().x + hit.getPosition().y*hit.getPosition().y);
-        graphPos[evNum][3]->SetPoint(nClusterHits, hit.getPosition().y, hit.getPosition().x);
-        graphPos[evNum][4]->SetPoint(nClusterHits, hit.getPosition().z, hit.getPosition().y);
-        graphPos[evNum][5]->SetPoint(nClusterHits, hit.getPosition().z, r);
-      }
-      nClusterHits++;
+      // Do we need to plot anything specific from the hits
+      // belonging to the clusters?
     }
 
-
-    h_clLayer->Fill(ch_layer);
     nClusters++;
   }
-
-  cleanTree();
+  t_clusters->Fill ();
 
   std::uint64_t nSeeds = 0;
   std::uint64_t nFollowers = 0;
   std::uint64_t nOutliers = 0;
   for (const auto& clue_hit : (clue_calo_coll->vect)) {
-    m_event->push_back (evNum);
+    m_hits_event->push_back (evNum);
     if(clue_hit.inBarrel()){
-      m_region->push_back (0);
+      m_hits_region->push_back (0);
     } else {
-      m_region->push_back (1);
+      m_hits_region->push_back (1);
     }
-    m_layer->push_back (clue_hit.getLayer());
-    m_x->push_back (clue_hit.getPosition().x);
-    m_y->push_back (clue_hit.getPosition().y);
-    m_z->push_back (clue_hit.getPosition().z);
-    m_eta->push_back (clue_hit.getEta());
-    m_phi->push_back (clue_hit.getPhi());
-    m_rho->push_back (clue_hit.getRho());
-    m_delta->push_back (clue_hit.getDelta());
-    m_energy->push_back (clue_hit.getEnergy());
+    m_hits_layer->push_back (clue_hit.getLayer());
+    m_hits_x->push_back (clue_hit.getPosition().x);
+    m_hits_y->push_back (clue_hit.getPosition().y);
+    m_hits_z->push_back (clue_hit.getPosition().z);
+    m_hits_eta->push_back (clue_hit.getEta());
+    m_hits_phi->push_back (clue_hit.getPhi());
+    m_hits_rho->push_back (clue_hit.getRho());
+    m_hits_delta->push_back (clue_hit.getDelta());
+    m_hits_energy->push_back (clue_hit.getEnergy());
 
     if(clue_hit.isFollower()){
-      m_status->push_back(1);
+      m_hits_status->push_back(1);
       nFollowers++;
     }
     if(clue_hit.isSeed()){
-      m_status->push_back(2);
+      m_hits_status->push_back(2);
       nSeeds++;
     }
 
     if(clue_hit.isOutlier()){
-      m_status->push_back(0);
+      m_hits_status->push_back(0);
       nOutliers++;
     }
   }
@@ -194,50 +131,74 @@ StatusCode CLUEHistograms::execute() {
   return StatusCode::SUCCESS;
 }
 
-void CLUEHistograms::initializeTree() {
+void CLUEHistograms::initializeTrees() {
 
-  m_event = new std::vector<int>();
-  m_region = new std::vector<int>();
-  m_layer = new std::vector<int>();
-  m_status = new std::vector<int>();
-  m_x = new std::vector<float>();
-  m_y = new std::vector<float>();
-  m_z = new std::vector<float>();
-  m_eta = new std::vector<float>();
-  m_phi = new std::vector<float>();
-  m_rho = new std::vector<float>();
-  m_delta = new std::vector<float>();
-  m_energy = new std::vector<float>();
+  m_hits_event = new std::vector<int>();
+  m_hits_region = new std::vector<int>();
+  m_hits_layer = new std::vector<int>();
+  m_hits_status = new std::vector<int>();
+  m_hits_x = new std::vector<float>();
+  m_hits_y = new std::vector<float>();
+  m_hits_z = new std::vector<float>();
+  m_hits_eta = new std::vector<float>();
+  m_hits_phi = new std::vector<float>();
+  m_hits_rho = new std::vector<float>();
+  m_hits_delta = new std::vector<float>();
+  m_hits_energy = new std::vector<float>();
 
-  t_hits->Branch ("event", &m_event);
-  t_hits->Branch ("region", &m_region);
-  t_hits->Branch ("layer", &m_layer);
-  t_hits->Branch ("status", &m_status);
-  t_hits->Branch ("x", &m_x);
-  t_hits->Branch ("y", &m_y);
-  t_hits->Branch ("z", &m_z);
-  t_hits->Branch ("eta", &m_eta);
-  t_hits->Branch ("phi", &m_phi);
-  t_hits->Branch ("rho", &m_rho);
-  t_hits->Branch ("delta", &m_delta);
-  t_hits->Branch ("energy", &m_energy);
+  t_hits->Branch ("event", &m_hits_event);
+  t_hits->Branch ("region", &m_hits_region);
+  t_hits->Branch ("layer", &m_hits_layer);
+  t_hits->Branch ("status", &m_hits_status);
+  t_hits->Branch ("x", &m_hits_x);
+  t_hits->Branch ("y", &m_hits_y);
+  t_hits->Branch ("z", &m_hits_z);
+  t_hits->Branch ("eta", &m_hits_eta);
+  t_hits->Branch ("phi", &m_hits_phi);
+  t_hits->Branch ("rho", &m_hits_rho);
+  t_hits->Branch ("delta", &m_hits_delta);
+  t_hits->Branch ("energy", &m_hits_energy);
+
+  m_clusters_event = new std::vector<int>();
+  m_clusters_layer = new std::vector<int>();
+  m_clusters_size = new std::vector<int>();
+  m_clusters_x = new std::vector<float>();
+  m_clusters_y = new std::vector<float>();
+  m_clusters_z = new std::vector<float>();
+  m_clusters_energy = new std::vector<float>();
+
+  t_clusters->Branch ("event", &m_clusters_event);
+  t_clusters->Branch ("layer", &m_clusters_layer);
+  t_clusters->Branch ("size", &m_clusters_size);
+  t_clusters->Branch ("x", &m_clusters_x);
+  t_clusters->Branch ("y", &m_clusters_y);
+  t_clusters->Branch ("z", &m_clusters_z);
+  t_clusters->Branch ("energy", &m_clusters_energy);
 
   return;
 }
 
-void CLUEHistograms::cleanTree() {
-  m_event->clear();
-  m_region->clear(); 
-  m_layer->clear();
-  m_status->clear();
-  m_x->clear();
-  m_y->clear();
-  m_z->clear();
-  m_eta->clear();
-  m_phi->clear();
-  m_rho->clear();
-  m_delta ->clear();
-  m_energy ->clear();
+void CLUEHistograms::cleanTrees() {
+  m_hits_event->clear();
+  m_hits_region->clear(); 
+  m_hits_layer->clear();
+  m_hits_status->clear();
+  m_hits_x->clear();
+  m_hits_y->clear();
+  m_hits_z->clear();
+  m_hits_eta->clear();
+  m_hits_phi->clear();
+  m_hits_rho->clear();
+  m_hits_delta ->clear();
+  m_hits_energy ->clear();
+
+  m_clusters_event->clear();
+  m_clusters_layer->clear();
+  m_clusters_size->clear();
+  m_clusters_x->clear();
+  m_clusters_y->clear();
+  m_clusters_z->clear();
+  m_clusters_energy ->clear();
 
   return;
 }
