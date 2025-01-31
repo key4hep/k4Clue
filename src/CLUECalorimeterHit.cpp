5f36f527c54aefca243d6e48ea43ff428994f057
AuroraPerego <aurora.perego@cern.ch>
2025-01-31 11:57:50 +0100

    restructure CLUECalorimeterHit and add operator <<

--- src/CLUECalorimeterHit.cpp
+++ src/CLUECalorimeterHit.cpp
@@ -38,24 +38,25 @@ CLUECalorimeterHit::CLUECalorimeterHit(const CalorimeterHit& ch, const CLUECalor
 CLUECalorimeterHit::CLUECalorimeterHit(const CalorimeterHit& ch, const CLUECalorimeterHit::DetectorRegion detRegion,
                                        const int layer, const CLUECalorimeterHit::Status status, const int clusterIndex,
                                        const float rho, const float delta)
-    : CalorimeterHit(ch), m_detectorRegion(detRegion), m_layer(layer), m_status(status), m_rho(rho), m_delta(delta),
+    : CalorimeterHit(ch), m_rho(rho), m_delta(delta), m_detectorRegion(detRegion), m_status(status), m_layer(layer),
       m_clusterIndex(clusterIndex) {
   setR();
   setEta();
   setPhi();
 }
 
-const std::uint64_t& CLUECalorimeterHit::getLayer() const { return m_layer; }
+uint32_t CLUECalorimeterHit::getLayer() const { return m_layer; }
 bool CLUECalorimeterHit::inBarrel() const { return (m_detectorRegion == barrel ? true : false); }
 bool CLUECalorimeterHit::inEndcap() const { return (m_detectorRegion == endcap ? true : false); }
 bool CLUECalorimeterHit::isFollower() const { return (m_status == follower ? true : false); }
 bool CLUECalorimeterHit::isSeed() const { return (m_status == seed ? true : false); }
 bool CLUECalorimeterHit::isOutlier() const { return (m_status == outlier ? true : false); }
-const float& CLUECalorimeterHit::getRho() const { return m_rho; }
-const float& CLUECalorimeterHit::getDelta() const { return m_delta; }
-const float& CLUECalorimeterHit::getR() const { return m_r; }
-const float& CLUECalorimeterHit::getEta() const { return m_eta; }
-const float& CLUECalorimeterHit::getPhi() const { return m_phi; }
+float CLUECalorimeterHit::getRho() const { return m_rho; }
+float CLUECalorimeterHit::getDelta() const { return m_delta; }
+float CLUECalorimeterHit::getR() const { return m_r; }
+float CLUECalorimeterHit::getEta() const { return m_eta; }
+float CLUECalorimeterHit::getPhi() const { return m_phi; }
+int32_t CLUECalorimeterHit::getClusterIndex() const { return m_clusterIndex; };
 
 void CLUECalorimeterHit::setEta() { m_eta = -1. * log(tan(atan2(m_r, getPosition().z) / 2.)); }
 
@@ -66,3 +67,20 @@ void CLUECalorimeterHit::setR() {
 }
 
 } // namespace clue
+
+std::ostream& operator<<(std::ostream& o, const clue::CLUECalorimeterHit& value) {
+  o << "Energy: " << value.getEnergy() << "\n"
+    << "Time: " << value.getTime() << "\n"
+    << "Position: (" << value.getPosition().x << ", " << value.getPosition().y << ", " << value.getPosition().z << ")\n"
+    << "Layer: " << value.getLayer() << "\n"
+    << "Region: " << (value.inBarrel() ? "Barrel" : "Endcap") << "\n"
+    << "Status: "
+    << (value.isSeed()       ? "Seed"
+        : value.isFollower() ? "Follower"
+                             : "Outlier")
+    << "\n"
+    << "Rho: " << value.getRho() << "\n"
+    << "Delta: " << value.getDelta() << "\n"
+    << "Cluster Index: " << value.getClusterIndex() << "\n";
+  return o;
+}
